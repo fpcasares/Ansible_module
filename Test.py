75b395ccae674a3b7abc95c510e41eba3217ebd7
Facundo Pablo Casares <fpcasares@gmail.com>
2017-11-23 17:54:31 -0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,28 +1,130 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
-import socket
-import json
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
 
-from pycsco.nxos.device import Device
-from pycsco.nxos.utils import nxapi_lib
+DOCUMENTATION = '''
+---
+module: config_compliance
 
+short_description: This module will perform configuration compliance
+
+version_added: "2.4"
+
+description:
+    - "This is my longer description explaining my sample module"
+
+options:
+    name:
+        description:
+            - This is the message to send to the sample module
+        required: true
+    new:
+        description:
+            - Control to demo if the result of this module is changed or not
+        required: false
+
+extends_documentation_fragment:
+    - azure
+
+author:
+    - Facundo Pablo Casares (@fpcasares)
+'''
+
+EXAMPLES = '''
+# Pass in a message
+- name: Test with a message
+  my_new_test_module:
+    name: hello world
+
+# pass in a message and have changed true
+- name: Test with a message and changed output
+  my_new_test_module:
+    name: hello world
+    new: true
+
+# fail the module
+- name: Test failure of the module
+  my_new_test_module:
+    name: fail me
+'''
+
+RETURN = '''
+original_message:
+    description: The original name param that was passed in
+    type: str
+message:
+    description: The output message that the sample module generates
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+
+def run_module():
+    # define the available arguments/parameters that a user can pass to
+    # the module
+    '''module_args = dict(
+        name=dict(type='str', required=True),
+        new=dict(type='bool', required=False, default=False)
+    )'''
+    module_args = dict(
+        commands=dict(type='list',required=False),
+        source_file=dict(type='str',required=False),
+        ordered=dict(
+        name=dict(type='str', required=True),
+        new=dict(type='bool', required=False, default=False)
+    )
+
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # change is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(
+        changed=False,
+        original_message='',
+        message=''
+    )
+
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    # if the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        return result
+
+    # manipulate or modify the state as needed (this is going to be the
+    # part where your module will do what it needs to do)
+    result['original_message'] = module.params['name']
+    result['message'] = 'goodbye'
+
+    # use whatever logic you need to determine whether or not this module
+    # made any modifications to your target
+    if module.params['new']:
+        result['changed'] = True
+
+    # during the execution of the module, if there is an exception or a
+    # conditional state that effectively causes a failure, run
+    # AnsibleModule.fail_json() to pass in the message and the result
+    if module.params['name'] == 'fail me':
+        module.fail_json(msg='You requested this to fail', **result)
+
+    # in the event of a successful module execution, you will want to
+    # simple AnsibleModule.exit_json(), passing the key/value results
+    module.exit_json(**result)
 
 def main():
+    run_module()
 
-    username = 'cisco'
-    password = 'cisco'
-    protocol = 'http'
-    node = 'n9k1'
-    host = socket.gethostbyname(node)
-
-    device = Device(ip=host, username=username, password=password,
-                    protocol=protocol)
-
-    neighbors = nxapi_lib.get_neighbors(device)
-
-    print neighbors   # returns a list
-
-    neighs = dict(neighbors=neighbors)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
